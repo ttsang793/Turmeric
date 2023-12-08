b19f63c0a1efa3a7fe861d5d54ffae561590a84c
lnbthao <152460684+lnbthao@users.noreply.github.com>
2023-12-08 15:13:47 +0700

    update giới thiệu

---
+++ Web1-main/js/head-foot.js
@@ -0,0 +1,128 @@
+document.getElementsByTagName("header")[0].innerHTML += `
+<div class="container">
+    <div class="text-end align-items-center">                
+        <i class="bi bi-facebook icon align-middle"></i>
+        <i class="bi bi-instagram icon align-middle"></i>
+        <i class="bi bi-pinterest icon align-middle"></i>
+        <i class="bi bi-envelope icon align-middle"></i>
+        <button class="btn" id="login">
+            <i class="bi bi-person-fill"></i>
+            <div id="user-login"></div>
+        </button>
+    </div>
+
+    <div class="row align-items-center">
+        <div class="col-3">
+            <img src="./img/logo1.png" alt="" class="logo">
+        </div>
+
+        <div class="col-8">
+            <div class="input-group">
+                <input type="search" placeholder="Tìm kiếm" class="search" id="search">
+                <div class="input-group-append">
+                    <button class="btn btn-warning" onclick="search()">
+                        <i class="bi bi-search"></i>
+                    </button>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-1">
+        
+            <a href="./cart.html" style="color: black">
+                <i class="bi bi-cart" style="font-size: 32px"></i>
+            </a>
+        </div>
+    </div>        
+</div>
+
+<div class="row text-center nav">
+    <div class="col-3"></div>
+    <div class="col-6 btn-group">
+        <a href="./index.html" class="btn btn-dark">
+            TRANG CHỦ
+        </a>
+        <a href="./san-pham.html" class="btn btn-dark">
+            SẢN PHẨM
+        </a>
+        <a href="./gioi-thieu.html" class="btn btn-dark">
+            GIỚI THIỆU
+        </a>
+        <a href="./blog.html" class="btn btn-dark">
+            BLOG
+        </a>
+    </div>
+    <div class="col-3"></div>
+</div>
+`
+
+document.getElementsByTagName("footer")[0].innerHTML = `
+<div class="container pb-2">
+    <div class="row">
+        <div class="col-4">
+            <h2>HỖ TRỢ</h2>
+            <ul>
+                <li>
+                    <a href="#">Chính sách vận chuyển</a>
+                </li>
+                <li>
+                    <a href="#">Hình thức thanh toán</a>
+                </li>
+                <li>
+                    <a href="#">Chính sách đổi trả</a>
+                </li>
+                <li>
+                    <a href="#">Report</a>
+                </li>
+            </ul>
+        </div>
+
+        <div class="col-4">
+            <h2>LIÊN HỆ</h2>
+            <ul>
+                <li>
+                    <a href=""><i class="bi bi-facebook"></i> turmeric_cosmestic</a>
+                </li>
+                <li>
+                    <a href=""><i class="bi bi-instagram"></i> _Turmeric.2022_</a>
+                </li>
+                <li>
+                    <a href=""><i class="bi bi-envelope"></i> Turmeric@gmail.com</a>
+                </li>
+                <li>
+                    <a href=""><i class="bi bi-telephone"></i> HOTLINE: 028 3827 9777</a>
+                </li>
+                </li>
+            </ul>
+        </div>
+
+        <div class="col-4">
+            <h2>DỊCH VỤ</h2>
+            <ul>
+                <li class="highlight"><i class="bi bi-door-open"></i> OPEN: Thứ 2 ~ Thứ 6 (trừ ngày lễ)</li>
+                <li class="highlight"><i class="bi bi-phone"></i> 0348923412</li>
+            </ul>
+        </div>
+    </div>
+    <hr class="my-1">
+    <div class="row">
+        <span class="text-center">&copy 2023 | Copyright by Turmeric</span>
+    </div>
+</div>
+`
+
+function search() {
+    const search = document.getElementById("search").value;
+    window.location = (search === "") ? "./san-pham.html" : `./san-pham.html?search=${search}`;
+}
+
+document.getElementById("search").addEventListener("keyup", event => {
+    if (event.key === "Enter") search();
+})
+
+function getGia(gia) {
+    return Number(gia).toLocaleString("vi-VN") + "đ"; //định dạng số 1.000.000 VN
+}
+
+const userLogin = ""; //lưu ý: username này chỉ có mục đích là kiểm thử
+document.getElementById("user-login").innerHTML = (userLogin === "") ? "Đăng nhập" : userLogin;
---
+++ Web1-main/js/productInfo.js
@@ -0,0 +1,77 @@
+const productView = document.querySelector(".product-view");
+
+function showInfo(id) {
+    product = getProduct(id);
+    $('.product-view').css("display", "block");
+    $('body').css("overflow", "hidden");
+
+
+    productView.innerHTML = `
+        <div class="container">
+            <button class="btn close" onclick="closeInfo()">&times;</button>
+            <div class="row p-5">
+                <div class="col-5">
+                    <img src="${product.img}" alt="" style="width:70%">
+                </div>
+                <div class="col-7">
+                    <fieldset class="tieu-de mb-5">
+                        <h2 class="mb-4">${product.name}</h2>
+                        <h3 style="color: palevioletred">${getGia(product.price)}</h3>
+                        <div class="input-group mb-3 pt-3">
+                            <button class="btn amount text-center" onclick="changeNumber('-')">-</button>
+                            <input id="amount-info" type="number" name="" id="" value="1" min="1" max="" class="ammount text-center">
+                            <button class="btn amount text-center" onclick="changeNumber('+')">+</button>
+                            <div class="btn btn-warning" onclick="addToCart(${product.id})">Thêm vào giỏ hàng</div>
+                        </div>
+                    </fieldset>
+
+                    <fieldset class="mb-3">
+                        <legend>Mô tả</legend>
+                        <p>
+                            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                            Sunt veniam perspiciatis id, numquam, aspernatur aliquam reiciendis pariatur unde
+                            asperiores laboriosam facere, ipsum dolorem odit sit? Ad vitae dicta laboriosam odio.
+                        </p>
+                    </fieldset>
+                </div>
+            </div>
+        </div>
+    `
+}
+
+function closeInfo() {
+    $('.product-view').css("display", "none");
+    $('body').css("overflow", "initial");
+
+    productView.innerHTML = '';
+}
+
+function changeNumber(input) {
+    ammount = document.getElementById("amount-info");
+    if (input === "+") ammount.value++;
+    else if (ammount.value > 1) ammount.value--;
+}
+
+function addToCart(product, index = undefined) {
+    if (userLogin === "") {
+        $('.alert-danger').css("display", "initial");
+        setTimeout(() => $('.alert-danger').css("display", "none"),3000);
+        return;
+    }
+    if (index === undefined)
+        amount = document.getElementById("amount-info");
+    else
+        amount = document.getElementsByClassName("amount-page")[index];
+
+    if(findProduct(product) === -1) {
+        insertCart(product, amount.value);
+        document.querySelector(".alert-success").innerHTML = "Thêm thành công sản phẩm";
+    }
+
+    else {
+        updateCart(product, amount.value);
+        document.querySelector(".alert-success").innerHTML = "Update thành công sản phẩm";
+    };
+    $('.alert-success').css("display", "initial");
+    setTimeout(() => $('.alert').css("display", "none"),3000);
+}
